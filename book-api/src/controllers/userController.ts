ed98b6dec2cb51f4ef61d52cc15abc8adbc32d47
Malin Rosenquist <malinrosenquist@outlook.com>
2025-05-08 10:40:49 +0200

    feat: add users route and controller for updating user by id

--- book-api/src/controllers/userController.ts
+++ book-api/src/controllers/userController.ts
@@ -28,5 +28,30 @@ export const fetchUser = async (req: Request, res: Response) => {
 };
 
 // UPDATE USER BY ID
+export const updateUser = async (req: Request, res: Response) => {
+  const { username, password, is_admin } = req.body;
+
+  try {
+    const updatedUser = await User.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          username: username,
+          password: password,
+          is_admin: is_admin,
+        },
+      }
+    );
+
+    if (updatedUser.matchedCount === 0) {
+      res.status(400).json({ success: false, message: "User not found" });
+      return;
+    }
+    res.json({ message: "User updated", data: await User.findById(req.params.id) });
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : "Unknown error";
+    res.status(500).json({ error: message });
+  }
+};
 
 // DELETE USER BY ID
--- book-api/src/routes/users.ts
+++ book-api/src/routes/users.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { fetchAllUsers, fetchUser } from "../controllers/userController";
+import { fetchAllUsers, fetchUser, updateUser } from "../controllers/userController";
 const router = express.Router();
 
 router.get("/", fetchAllUsers);
 router.get("/:id", fetchUser);
+router.patch("/:id", updateUser);
 
 export default router;
