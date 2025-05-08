4c93aaa3ed6c58aa2872e329c09678c641ccec68
Emelie Boss <Emelie.boss@medieinstitutet.se>
2025-05-08 16:24:16 +0200

    feat: add an interface and mongoose schema for books

---
+++ book-api/src/types/IBook.ts
@@ -0,0 +1,12 @@
+/**
+ * Represents a book entry.
+ */
+export interface IBook {
+    title: string;
+    description: string;
+    author: string;
+    genres: string[];
+    image: string;
+    published_year: number;
+  }
+  
