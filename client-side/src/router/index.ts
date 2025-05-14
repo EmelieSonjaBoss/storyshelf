bc79844a21b5185fd2b192c1b0289b92764f871a
Malin Rosenquist <malinrosenquist@outlook.com>
2025-05-14 12:26:32 +0200

    feat: add admin route with authentication guard

--- client-side/src/router/index.ts
+++ client-side/src/router/index.ts
@@ -4,6 +4,8 @@ import BookView from "../views/BookView.vue";
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import AdminView from "@/views/AdminView.vue";
+import { createPinia } from "pinia";
+import useAuthStore from "@/stores/useAuthStore";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,29 +13,43 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView
+      component: HomeView,
     },
     {
-      path: '/books/:id',
-      name: 'Book',
-      component: BookView
+      path: "/books/:id",
+      name: "Book",
+      component: BookView,
     },
     {
       path: "/login",
       name: "Login",
-      component: LoginView
+      component: LoginView,
     },
     {
       path: "/register",
       name: "Register",
-      component: RegisterView
+      component: RegisterView,
     },
     {
       path: "/admin",
       name: "Admin",
       component: AdminView,
+      meta: {
+        requiresAuth: true,
+      },
     },
   ],
+});
+
+const pinia = createPinia();
+const useAuth = useAuthStore(pinia);
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !useAuth.isAuthenticated) {
+    next("/login");
+  } else {
+    next();
+  }
 });
 
 export default router;
